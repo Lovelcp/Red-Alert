5b343f9e13254588ec39c0a83f8853fef40292b6
kissrain <cjyang1123@gmail.com>
2017-07-03 23:57:52 +0800

    console工程初始化

---
+++ red-alert-console/src/main/java/com/rainbow/red_alert/console/RedAlertConsoleApplication.java
@@ -0,0 +1,12 @@
+package com.rainbow.red_alert.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedAlertConsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedAlertConsoleApplication.class, args);
+    }
+}
---
+++ red-alert-console/src/test/java/com/rainbow/red_alert/console/RedAlertConsoleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rainbow.red_alert.console;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedAlertConsoleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
